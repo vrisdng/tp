58ddc37768e0d2ca61bf7255afa9d549e2ce6428
lilduckling <naduc3110@gmail.com>
2025-03-18 20:31:30 +0800

    Add more tests

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
@@ -92,5 +94,52 @@ public class ListCommandTest {
         assert DisplayPreferences.isShowAddress();
         assert DisplayPreferences.isShowStudentId();
         assert DisplayPreferences.isShowTags();
+    }
+
+    @Test
+    public void execute_updatesFilteredPersonList() {
+        // Test that the filtered person list is updated
+        ListCommand listCommand = new ListCommand(true, true, true, true, true, true);
+        assertCommandSuccess(listCommand, model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+
+        // Verify that the filtered list is updated
+        assertEquals(expectedModel.getFilteredPersonList(), model.getFilteredPersonList());
+    }
+
+    @Test
+    public void equals() {
+        // Test equality of ListCommand objects
+        ListCommand listCommand1 = new ListCommand(true, true, true, true, true, true);
+        ListCommand listCommand2 = new ListCommand(true, true, true, true, true, true);
+        ListCommand listCommand3 = new ListCommand(false, true, true, true, true, true);
+
+        // Same values -> returns true
+        assertEquals(listCommand1, listCommand2);
+
+        // Different values -> returns false
+        assertNotEquals(listCommand1, listCommand3);
+
+        // Same object -> returns true
+        assertEquals(listCommand1, listCommand1);
+
+        // Null -> returns false
+        assertNotEquals(listCommand1, null);
+
+        // Different types -> returns false
+        assertNotEquals(listCommand1, 5);
+    }
+
+    @Test
+    public void hashCode_test() {
+        // Test hashCode implementation
+        ListCommand listCommand1 = new ListCommand(true, true, true, true, true, true);
+        ListCommand listCommand2 = new ListCommand(true, true, true, true, true, true);
+        ListCommand listCommand3 = new ListCommand(false, true, true, true, true, true);
+
+        // Same values -> same hashCode
+        assertEquals(listCommand1.hashCode(), listCommand2.hashCode());
+
+        // Different values -> different hashCode
+        assertNotEquals(listCommand1.hashCode(), listCommand3.hashCode());
     }
 }
