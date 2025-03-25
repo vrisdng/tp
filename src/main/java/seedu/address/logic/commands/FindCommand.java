ff8d843ccdbb449e197f83e3ad88211a4dc5fca5
lilduckling <naduc3110@gmail.com>
2025-03-25 15:44:22 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -22,7 +22,7 @@ public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
         + ": Finds all students whose specified field contains any of "
         + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
         + "Parameters: PREFIX/KEYWORD [MORE_KEYWORDS]...\n"
