3f6a2a73d1537c937209963375450ab5c7627964
Nguyen Anh Duc <nguyen_anh_duc@u.nus.edu>
2025-03-31 16:12:29 +0800

    Find and delete command to find and delete by anything

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -7,17 +7,14 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.StudentId;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
@@ -28,12 +25,11 @@ public class DeleteCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_validStudentIdUnfilteredList_success() {
+    public void execute_validNameUnfilteredList_success() {
         Person personToDelete = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        DeleteCommand deleteCommand = new DeleteCommand(personToDelete.getStudentId());
+        DeleteCommand deleteCommand = new DeleteCommand("n/", personToDelete.getName().fullName);
 
-        String expectedMessage = String.format(DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS,
-                Messages.format(personToDelete));
+        String expectedMessage = String.format(DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS, 1);
 
         ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
         expectedModel.deletePerson(personToDelete);
@@ -42,22 +38,42 @@ public class DeleteCommandTest {
     }
 
     @Test
-    public void execute_invalidStudentIdUnfilteredList_throwsCommandException() {
-        StudentId invalidStudentId = new StudentId("A0000000Z");
-        DeleteCommand deleteCommand = new DeleteCommand(invalidStudentId);
-        assertCommandFailure(deleteCommand, model,
-                String.format(DeleteCommand.MESSAGE_STUDENT_NOT_FOUND, invalidStudentId));
+    public void execute_validTagUnfilteredList_success() {
+        // Get the tag of the first person in the list
+        Person personToDelete = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        String tagToDelete = personToDelete.getTags().iterator().next().tagName; // Get the first tag
+
+        // Create the DeleteCommand
+        DeleteCommand deleteCommand = new DeleteCommand("t/", tagToDelete);
+
+        // Count the number of persons with the matching tag
+        long matchingPersonsCount = model.getFilteredPersonList().stream()
+                .filter(person -> person.getTags().stream()
+                        .anyMatch(tag -> tag.tagName.equals(tagToDelete)))
+                .count();
+
+        // Update the expected message to reflect the number of deletions
+        String expectedMessage = String.format(DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS, matchingPersonsCount);
+
+        // Create the expected model
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        model.getFilteredPersonList().stream()
+                .filter(person -> person.getTags().stream()
+                        .anyMatch(tag -> tag.tagName.equals(tagToDelete)))
+                .forEach(expectedModel::deletePerson);
+
+        // Assert that the command succeeds and deletes all matching persons
+        assertCommandSuccess(deleteCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
-    public void execute_validIndexFilteredList_success() {
+    public void execute_validStudentIdFilteredList_success() {
         showPersonAtIndex(model, INDEX_FIRST_PERSON);
 
         Person personToDelete = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        DeleteCommand deleteCommand = new DeleteCommand(personToDelete.getStudentId());
+        DeleteCommand deleteCommand = new DeleteCommand("s/", personToDelete.getStudentId().value);
 
-        String expectedMessage = String.format(DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS,
-                Messages.format(personToDelete));
+        String expectedMessage = String.format(DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS, 1);
 
         Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
         expectedModel.deletePerson(personToDelete);
@@ -70,43 +86,38 @@ public class DeleteCommandTest {
     public void execute_invalidStudentIdFilteredList_throwsCommandException() {
         showPersonAtIndex(model, INDEX_FIRST_PERSON);
 
-        Person secondPerson = model.getAddressBook().getPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
-        DeleteCommand deleteCommand = new DeleteCommand(secondPerson.getStudentId());
+        DeleteCommand deleteCommand = new DeleteCommand("s/", "A0000000Z");
 
         assertCommandFailure(deleteCommand, model,
-                String.format(DeleteCommand.MESSAGE_STUDENT_NOT_FOUND, secondPerson.getStudentId()));
+                String.format(DeleteCommand.MESSAGE_NO_PERSON_FOUND, "s/ A0000000Z"));
     }
 
     @Test
     public void equals() {
-        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        Person secondPerson = model.getFilteredPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
-        DeleteCommand deleteFirstCommand = new DeleteCommand(firstPerson.getStudentId());
-        DeleteCommand deleteSecondCommand = new DeleteCommand(secondPerson.getStudentId());
+        DeleteCommand deleteNameCommand = new DeleteCommand("n/", "Alice");
+        DeleteCommand deleteTagCommand = new DeleteCommand("t/", "friends");
 
         // same object -> returns true
-        assertTrue(deleteFirstCommand.equals(deleteFirstCommand));
+        assertTrue(deleteNameCommand.equals(deleteNameCommand));
 
         // same values -> returns true
-        DeleteCommand deleteFirstCommandCopy = new DeleteCommand(firstPerson.getStudentId());
-        assertTrue(deleteFirstCommand.equals(deleteFirstCommandCopy));
+        DeleteCommand deleteNameCommandCopy = new DeleteCommand("n/", "Alice");
+        assertTrue(deleteNameCommand.equals(deleteNameCommandCopy));
 
         // different types -> returns false
-        assertFalse(deleteFirstCommand.equals(1));
+        assertFalse(deleteNameCommand.equals(1));
 
         // null -> returns false
-        assertFalse(deleteFirstCommand.equals(null));
+        assertFalse(deleteNameCommand.equals(null));
 
-        // different person -> returns false
-        assertFalse(deleteFirstCommand.equals(deleteSecondCommand));
+        // different commands -> returns false
+        assertFalse(deleteNameCommand.equals(deleteTagCommand));
     }
 
     @Test
     public void toStringMethod() {
-        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        DeleteCommand deleteCommand = new DeleteCommand(firstPerson.getStudentId());
-        String expected = DeleteCommand.class.getCanonicalName() + "{targetStudentId="
-                + firstPerson.getStudentId() + "}";
+        DeleteCommand deleteCommand = new DeleteCommand("n/", "Alice");
+        String expected = DeleteCommand.class.getCanonicalName() + "{field=n/, keyword=Alice}";
         assertEquals(expected, deleteCommand.toString());
     }
 
